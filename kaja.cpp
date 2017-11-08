0f41e010e416d90103b3e50ae96e95d386fad7f1
karolinamaruszak <32893258+karolinamaruszak@users.noreply.github.com>
2017-11-08 09:53:12 +0100

    Add files via upload

--- kaja.cpp
+++ kaja.cpp
@@ -6,25 +6,23 @@ using namespace std;
 int main()
 {
 	
-	 int tab[10]={4,6,7,3,4,8,9,10,3,5};
+	 int tab[10]={4,6,7,3,0,8,9,10,1,5};
 	 
 	 
 	 int min,max;
-	 min=tab[0]; 
-	min=tab[0];
-	
+	 
 	
  
-  for( int i=0;i<10 ; i++)
-     { 
-		 
-	   if ( tab[i] > max) max=tab[i];
-	   
-	   if ( tab[i]<min ) min=tab[i];
-	   
-		
- }
- cout<<min<<endl<<max;
- 
+ for( int i=0;i<10;i++)
+ { min=tab[i];
+	 for(int j=0;j<9;j++)
+	  { 
+		 if(tab[j]<min ) min=tab[j];
+		 {
+	 `
+for(int i=0;i<10;i++)
+{ 
+ cout << tab[i]<<" ";
+}
  return 0;
 }
